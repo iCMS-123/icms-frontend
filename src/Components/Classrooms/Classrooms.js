db1281931859cf6374d218f9eaea58dee6928935
Sujal Gupta <sujalgupta6100@gmail.com>
2023-02-13 13:18:28 +0530

    Create Classroom
    Signed-off-by: Sujal Gupta <sujalgupta6100@gmail.com>

--- src/Components/Classrooms/Classrooms.js
+++ src/Components/Classrooms/Classrooms.js
@@ -1,17 +1,61 @@
+import { Form } from "react-bootstrap";
+
+const Classrooms = () => {
+  return (
+    <div className="container">
+      <h1 className="py-4"> Create a class</h1>
+      <Form style={formStyles}>
+
+      <div style={selectTagStyles}>
 
 
-const Classrooms = ()=>{
+        <p>Select Year</p>
+        <Form.Select
+          
+          aria-label="Default select example"
+        >
+          <option defaultValue value="1">  1<sup>st</sup> Year</option>
+          <option value="2"> 2<sup>nd</sup> Year</option>
+          <option value="3">  3<sup>rd</sup> Year</option>
+          <option value="4">  4<sup>th</sup> Year</option>
+        </Form.Select>
+      </div> 
+        <div style={selectTagStyles}>
+        <p>Name of  the class</p>
+       
+        <Form.Group className="mb-2" controlId="formClassName">
+          <Form.Control required type="text" placeholder="Enter Class Name" />
+        </Form.Group>
+        </div>
 
 
-    return <div>
+        <div style={selectTagStyles}>
+        <p>Assign a class coordinator</p>
+        <Form.Select
+           
+          aria-label="Default select example"
+        >
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </Form.Select>
 
-
-    ClassRooms
-
-
+            
+        </div>
+      
+      </Form>
     </div>
-
-
-}
+  );
+};
 
 export default Classrooms;
+
+const formStyles = {
+  display: "flex",
+  justifyContent:'space-around',
+};
+
+let selectTagStyles = { marginRight: "10px" };
+ selectTagStyles = {
+    width:'30%'
+};
