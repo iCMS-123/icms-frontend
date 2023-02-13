198afc34d406b50e3f302e88e1d3761d30c52c94
Sujal Gupta <sujalgupta6100@gmail.com>
2023-02-13 13:19:30 +0530

    Remove Table
    Signed-off-by: Sujal Gupta <sujalgupta6100@gmail.com>

--- src/Components/StudentsInfo/StudentsInfo.js
+++ src/Components/StudentsInfo/StudentsInfo.js
@@ -26,50 +26,7 @@ const StudentsInfo = () => {
   return (
     <div>
       <section className="student-count">
-        <Table className="text-center" striped bordered hover variant="dark">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Year</th>
-              <th>Number of Students</th>
-              {/* <th>Username</th> */}
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>
-                1<sup>st</sup> Year
-              </td>
-              <td>{firstYearVal}</td>
-              {/* <td>@mdo</td> */}
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>
-                2<sup>nd</sup> Year
-              </td>
-              <td>{secondYearVal}</td>
-              {/* <td>@fat</td> */}
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>
-                3<sup>rd</sup> Year
-              </td>
-              <td>{thirdYearVal}</td>
-              {/* <td>@twitter</td> */}
-            </tr>
-            <tr>
-              <td>4</td>
-              <td>
-                4<sup>th</sup> Year
-              </td>
-              <td>{fourthYearVal}</td>
-              {/* <td>@twitter</td> */}
-            </tr>
-          </tbody>
-        </Table>
+        
       </section>
     </div>
   );
