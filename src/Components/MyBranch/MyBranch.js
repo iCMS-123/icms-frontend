c9d8f0144131e3ccfb8cebeac2455b1fcc1e4a08
Sujal Gupta <sujalgupta6100@gmail.com>
2023-05-28 16:57:24 +0530

    add verified and unverified teachers

--- src/Components/MyBranch/MyBranch.js
+++ src/Components/MyBranch/MyBranch.js
@@ -50,6 +50,9 @@ const MyBranch = (props) => {
   const [issuesData, setIssuesData] = useState([]); // for issues data
   const [activeIssues, setActiveIssues] = useState([]);
   const [resolvedIssues, setResolvedIssues] = useState([]);
+  const [allTeachersData, setAllTeachersData] = useState([]);
+  const [verifiedTeachers, setVerifiedTeachers] = useState([]);
+  const [unverifiedTeachers, setUnverifiedTeachers] = useState([]);
   // For issue Modal
   const [showIssueModal, setShowIssueModal] = useState(false);
   const [issueModalData, setIssueModalData] = useState({});
@@ -84,6 +87,15 @@ const MyBranch = (props) => {
     getClassroomsList();
   }, []);
 
+  useEffect(()=>{
+    setActiveIssues(issuesData.filter((issue) => !issue.isAttended))
+    setResolvedIssues(issuesData.filter((issue) => issue.isAttended))
+  }, [issuesData]);
+
+  useEffect(()=>{
+      setVerifiedTeachers(allTeachersData.filter((teacher)=>teacher.isVerified));
+      setUnverifiedTeachers(allTeachersData.filter((teacher)=>!teacher.isVerified));
+  }, [allTeachersData])
   useEffect(() => {
     const getIssuesList = async () => {
       try {
@@ -91,9 +103,9 @@ const MyBranch = (props) => {
 
         if (data && data.success) {
           let allIssues = data.data.branchData[0].issues;
-          setIssuesData(allIssues); // for issues data    
-          setActiveIssues(allIssues.filter((issue) => !issue.isAttended))
-          setResolvedIssues(allIssues.filter((issue) => issue.isAttended))
+          let allTeachers = data.data.branchData[0].teachers;
+          setIssuesData(allIssues); // for issues data      
+          setAllTeachersData(allTeachers);
         }
       } catch (e) {
         console.log(e, "e");
@@ -134,6 +146,7 @@ const MyBranch = (props) => {
         // console.log(data.data.id, "section id");
         setSectionId(data.data.id);
         setClassroomCardDetailsForModal(data.data);
+        console.log(data.data, "all section data");
       }
     } catch (e) {
       console.log(e, "e");
@@ -268,10 +281,12 @@ const MyBranch = (props) => {
       setUnverifiedTeachersListCopy(null);
   }
 
-  async function getTeacherDetailsAndShowInModal(idx) {
-    // console.log('getTeacherDetailsAndShowInModal');
-    console.log(unverifiedTeachersList, "unverifiedTeachersList");
-    setTeacherDetailsForModal(unverifiedTeachersListCopy[idx]);
+  async function getTeacherDetailsAndShowInModal(idx, isVerified) {
+    if(isVerified){
+      setTeacherDetailsForModal(verifiedTeachers[idx]);
+    }else{
+      setTeacherDetailsForModal(unverifiedTeachers[idx]);
+    }
     setTeacherDetailsModalShow(true);
   }
 
@@ -300,8 +315,9 @@ const MyBranch = (props) => {
     }
   }
   async function handleIssueStatusModal(decision) {
+    // console.log(issuesData, "allIssues");
     try {
-      const { data } = await axios.put(`http://localhost:8002/api/v1/section/resolve-issue/${userID}`, {
+      const { data } = await axios.put(`http://localhost:8002/api/v1/branch/resolve-issue/${userID}`, {
         issueId: issueModalData._id,
         status: decision
       })
@@ -318,8 +334,6 @@ const MyBranch = (props) => {
       }
       let allIssues = data.data.issues;
       setIssuesData(allIssues);
-      setActiveIssues(allIssues.filter((issue) => !issue.isAttended))
-      setResolvedIssues(allIssues.filter((issue) => issue.isAttended))
       } catch (err) {
       console.log(err);
     }
@@ -717,10 +731,10 @@ const MyBranch = (props) => {
 
 
 
-      {/* Unverified Teachers start */}
-      <div id={styles.unverifiedBox}>
-        <h5 style={{ fontWeight: 'bold' }}>Unverified Teachers</h5>
-        {(unverifiedTeachersListCopy == null) && <p className='mt-3'>Currently there are no such unverified teachers.</p>}
+      {/* Verified Teachers start */}
+      <div id={styles.verifiedBox}>
+        <h5 style={{ fontWeight: 'bold' }}>Verified Teachers</h5>
+        {(verifiedTeachers?.length < 1) && <p className='mt-3'>Currently there are no verified teachers.</p>}
 
         <Row>
           <Col xs={9}>
@@ -730,9 +744,8 @@ const MyBranch = (props) => {
 
             <Row xs={1} md={2} className="g-4" id={styles.teachersDiv}>
 
-              {(unverifiedTeachersListCopy != null) &&
-
-                unverifiedTeachersListCopy.map((teacher, index) => (
+              {
+                verifiedTeachers?.map((teacher, index) => (
                   <Col>
                     <Card>
                       <Card.Body>
@@ -740,16 +753,157 @@ const MyBranch = (props) => {
                           {teacher.isHod && <Badge bg="dark" style={{ position: 'absolute', top: '10px', right: '30px' }}>
                             HOD
                           </Badge>}
-                          {teacher.isVerified && <FaUserCheck style={{ position: 'absolute', top: '10px', right: '10px', color: 'green' }} />}
-                          {!teacher.isVerified && <FaUserTimes style={{ position: 'absolute', top: '10px', right: '10px', color: 'red' }} />}
-
+                          <FaUserCheck style={{ position: 'absolute', top: '10px', right: '10px', color: 'green' }} />
                           <Image src={teacher.profileImg} rounded={true} style={{ height: "90px", width: 'auto' }} className="me-3" />
                           <span style={{ display: 'flex', flexDirection: 'column' }}>
                             <b className="text-muted">{teacher.branchName.toUpperCase()}</b>
                             <b className="mb-2">{teacher.firstName + " " + teacher.lastName}</b>
                             <span>
                               <Button variant="outline-info" size="sm"
-                                onClick={e => getTeacherDetailsAndShowInModal(index)}
+                                onClick={e => getTeacherDetailsAndShowInModal(index, 1)}
+                              >
+                                View Details
+                              </Button>
+                              <Button variant="success" size="sm" style={{ width: 'fit-content', marginLeft: '15px' }}
+                                onClick={e => setTeacherVerified(teacher._id)}
+                              >
+                                <FaCheck />
+                              </Button>
+                            </span>
+                          </span>
+                        </div>
+                        {/* <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link> */}
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))
+              }
+            </Row>
+            {/* Modal code starts */}
+            <Modal show={teacherDetailsModalShow} fullscreen={true} onHide={() => setTeacherDetailsModalShow(false)}>
+              <Modal.Header closeButton>
+                {/* <Modal.Title>Teacher's Details</Modal.Title> */}
+              </Modal.Header>
+              <Modal.Body>
+                <div style={{ display: 'flex', width: '100%', height: '100%' }}>
+                  <div id='left-section' style={{ width: '30%', background: 'linear-gradient(90deg, #1CB5E0 0%, #000851 100%)', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', color: 'white' }}>
+                    <h5>
+                      {teacherDetailsForModal.isHod && <Badge bg="secondary" style={{ marginBottom: '20px' }}>
+                        HOD
+                      </Badge>}
+                    </h5>
+                    <Image src={teacherDetailsForModal.profileImg} roundedCircle={true} style={{ height: "100px", width: '100px' }} className="mb-3" />
+                    <h5>{teacherDetailsForModal.firstName + " " + teacherDetailsForModal.lastName}</h5>
+                    <h5> {teacherDetailsForModal.branchName?.toUpperCase()}</h5>
+                  </div>
+                  <div id='right-section' style={{ width: '70%', padding: '10px 20px' }}>
+                    <h6>Information</h6>
+                    <hr />
+                    <div style={{ padding: '10px 20px' }}>
+                      <Row>
+                        <Col style={{ display: 'flex', flexDirection: 'column' }}>
+                          <h6>First Name </h6>
+                          <p className='text-muted'>{teacherDetailsForModal.firstName}</p>
+                        </Col>
+                        <Col style={{ display: 'flex', flexDirection: 'column' }}>
+                          <h6>Last Name </h6>
+                          <p className='text-muted'>{teacherDetailsForModal.lastName}</p>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col style={{ display: 'flex', flexDirection: 'column' }}>
+                          <h6>Email </h6>
+                          <p className='text-muted'>{teacherDetailsForModal.email}</p>
+                        </Col>
+                        <Col style={{ display: 'flex', flexDirection: 'column' }}>
+                          <h6>Verification Status</h6>
+
+                          {teacherDetailsForModal.isVerified &&
+                            <p className='text-muted'>Verified
+                              <FaUserCheck style={{ marginLeft: '10px', color: 'green' }} />
+                            </p>
+                          }
+                          {!teacherDetailsForModal.isVerified &&
+                            <p className='text-muted'>Not Verified
+                              <FaUserTimes style={{ marginLeft: '10px', color: 'red' }} />
+                            </p>
+                          }
+
+                        </Col>
+                      </Row>
+                      <Col style={{ display: 'flex', flexDirection: 'column' }}>
+                        <h6>College ID</h6>
+                        <Image src={teacherDetailsForModal.collegeIdCard || 'https://picturedensity.com/wp-content/uploads/2019/06/Polytechnicollege-id-card.jpg'} style={{ height: "auto", width: 'auto', maxWidth: '300px' }} className="mt-2" />
+                      </Col>
+
+                    </div>
+
+                  </div>
+                </div>
+
+              </Modal.Body>
+              {/* <Modal.Footer>
+                <Button variant="success" onClick={e => setTeacherVerified(teacherDetailsForModal._id)}>Verify Account</Button>
+                <Button variant="dark" onClick={e => terminateAccount(teacherDetailsForModal._id)}>Terminate Account</Button>
+              </Modal.Footer> */}
+            </Modal>
+            {/* Modal code ends */}
+
+          </Col>
+          <Col className="ms-2 mt-0">
+            <h5>Search Teachers</h5>
+            <InputGroup className="mb-3">
+              <Form.Control
+                placeholder="Enter Teacher's Name"
+                aria-label="searchBox"
+                aria-describedby="basic-addon2"
+                onChange={e => { filterByName(e.target.value) }}
+              />
+              <InputGroup.Text id="basic-addon2"> <FaSearch /> </InputGroup.Text>
+            </InputGroup>
+
+
+          </Col>
+        </Row>
+
+      </div>
+      {/* Verified Teachers end */}
+      {/* Unverified Teachers start */}
+      <div id={styles.unverifiedBox}>
+        <h5 style={{ fontWeight: 'bold' }}>Unverified Teachers</h5>
+        {(unverifiedTeachers?.length < 1) && <p className='mt-3'>Currently there are no such unverified teachers.</p>}
+
+        <Row>
+          <Col xs={9}>
+            {loadingForFilter ? (
+              <Loader1></Loader1>
+            ) : ('')}
+
+            <Row xs={1} md={2} className="g-4" id={styles.teachersDiv}>
+
+              {
+
+                unverifiedTeachers?.map((teacher, index) => (
+                  <Col>
+                    <Card>
+                      <Card.Body>
+                        <div style={{ display: 'flex', }}>
+                          {teacher.isHod && <Badge bg="dark" style={{ position: 'absolute', top: '10px', right: '30px' }}>
+                            HOD
+                          </Badge>}
+                          <FaUserTimes style={{ position: 'absolute', top: '10px', right: '10px', color: 'red' }} />
+                          <Image src={teacher.profileImg} rounded={true} style={{ height: "90px", width: 'auto' }} className="me-3" />
+                          <span style={{ display: 'flex', flexDirection: 'column' }}>
+                            <b className="text-muted">{teacher.branchName.toUpperCase()}</b>
+                            <b className="mb-2">{teacher.firstName + " " + teacher.lastName}</b>
+                            <span>
+                              <Button variant="outline-info" size="sm"
+                                onClick={e => getTeacherDetailsAndShowInModal(index, 0)}
                               >
                                 View Details
                               </Button>
